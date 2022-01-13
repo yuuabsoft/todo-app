ea4f0de31a20c9586b52d7413d9288103403078a
yuuabsoft <yuki.ishikawa@nextbeat.net>
2022-01-13 10:18:32 +0900

    レスポンスを非同期で返すように修正

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -5,30 +5,31 @@ import model.ViewValueTodoList
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class TodoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = Action { implicit req =>
+  def index() = Action.async { implicit req =>
 
-    val todoList = Await.result(onMySQL.TodoRepository.all(), Duration.Inf)
-    val categoryList = Await.result(onMySQL.CategoryRepository.all(), Duration.Inf)
-
-    val todoViewList = for {
-      todo <- todoList
-      category <- categoryList.find(_.id == todo.v.categoryId)
+    for {
+      todoList <- onMySQL.TodoRepository.all()
+      categoryList <- onMySQL.CategoryRepository.all()
     } yield {
-      (todo, category)
-    }
+      val todoViewList = for {
+        todo <- todoList
+        category <- categoryList.find(_.id == todo.v.categoryId)
+      } yield {
+        (todo, category)
+      }
 
-    val vv = ViewValueTodoList(
-      title  = "Todoリスト",
-      cssSrc = Seq("main.css"),
-      jsSrc  = Seq("main.js"),
-      todoList = todoViewList
-    )
-    Ok(views.html.todo.TodoList(vv))
+      val vv = ViewValueTodoList(
+        title = "Todoリスト",
+        cssSrc = Seq("main.css"),
+        jsSrc = Seq("main.js"),
+        todoList = todoViewList
+        )
+      Ok(views.html.todo.TodoList(vv))
+    }
   }
 }
